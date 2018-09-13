4bad86f55ed3258b6f95abc45714a30388aef21a
Xavier Gillard <xavier.gillard@uclouvain.be>
2018-09-13 16:08:22 +0200

    range

---
+++ src/main/java/be/uclouvain/solvercheck/utils/collections/Range.java
@@ -0,0 +1,94 @@
+package be.uclouvain.solvercheck.utils.collections;
+
+import java.util.AbstractSet;
+import java.util.Iterator;
+
+/** An inclusive range of value: [lower; upper] */
+public final class Range extends AbstractSet<Integer> {
+    /** The lower bound of the range */
+    private final int from;
+    /** The upper bound of the range */
+    private final int to;
+    /** The step size between two consecutive elements */
+    private final int step;
+
+    /**
+     * Creates the given range of values spanning from `from` (inclusive)
+     * until `to` (inclusive) stepping by one.
+     *
+     * @param from the lower bound (incl.) of the set
+     * @param to the upper bound (incl.) of the set
+     */
+    public Range(final int from, final int to){
+        this(from, to, 1);
+    }
+
+    /**
+     * Creates the given range of values spanning from `from` (inclusive)
+     * until `to` (inclusive) stepping by `step` items.
+     *
+     * @param from the lower bound (incl.) of the set
+     * @param to the upper bound (incl.) of the set
+     * @param step the step size between any two elements of the set
+     */
+    public Range(final int from, final int to, final int step){
+        if(! isFeasible(from, to, step)){
+            throw new IllegalArgumentException("The range ["+from+";"+to+":"+step+"] has an infinite size");
+        }
+        this.from = from;
+        this.to   = to;
+        this.step = step;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int size() {
+        return (to-from)/step;
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * .. Note::
+     *    The order in which the items are enumerated is guaranteed to
+     *    follow the natural order of the integer. With items separated
+     *    by `step` elements.
+     */
+    @Override
+    public Iterator<Integer> iterator() {
+        return new RangeIterator();
+    }
+
+    /**
+     * Tells whether or not a range specified with the given parameters is closed.
+     * That is to say, it has a finite size
+     */
+    private static boolean isFeasible(final int from, final int to, final int step) {
+        return ( (from <= to && step > 0) || (from >= to && step < 0 ) );
+    }
+
+    /** An iterator that efficiently goes through all the values in the range */
+    private class RangeIterator implements Iterator<Integer> {
+        /** The current value of the iterator */
+        private int current;
+
+        /** Creates a new iterator to go over the current range */
+        public RangeIterator() {
+            this.current = from;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public boolean hasNext(){
+            return step > 0 ? current <= to : current >= to;
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public Integer next(){
+            int result = current;
+            current += step;
+            return result;
+        }
+    }
+}
---
+++ src/test/java/be/uclouvain/solvercheck/utils/TestRange.java
@@ -0,0 +1,49 @@
+package be.uclouvain.solvercheck.utils;
+
+import be.uclouvain.solvercheck.utils.collections.Range;
+import org.junit.Assert;
+import org.junit.Test;
+import org.quicktheories.WithQuickTheories;
+
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+public class TestRange implements WithQuickTheories {
+
+    @Test
+    public void oneCannotCreateAnOpenRange(){
+        qt().withGenerateAttempts(10000)
+            .forAll(integers().all(), integers().all(), integers().all())
+            .assuming((from, to, step) -> step == 0 || step > 0 && from > to || step < 0 && from < to)
+            .checkAssert((from, to, step) ->
+                assertThat(catchThrowable(()-> new Range(from, to, step)))
+                    .isInstanceOf(IllegalArgumentException.class)
+            );
+    }
+
+    @Test
+    public void testRangeSize(){
+        qt().withGenerateAttempts(10000)
+            .forAll(integers().all(), integers().all(), integers().all())
+            .assuming((from, to, step) -> (from <= to && step > 0) || (from > to && step < 0))
+            .check((from, to, step) ->
+                    new Range(from, to, step).size() ==
+                            (int) new Range(from, to, step).stream().count()
+            );
+    }
+
+    @Test
+    public void testRangeIterator() {
+        qt().withGenerateAttempts(10000)
+            .forAll(integers().between(-100, 100), integers().between(-100, 100), integers().between(-100, 100))
+            .assuming((from, to, step) -> (from <= to && step > 0) || (from > to && step < 0))
+            .checkAssert((from, to, step) -> {
+                Range tested = new Range(from, to, step);
+                for(int i = from; i <= to; i+= step) {
+                    Assert.assertTrue(tested.contains(i));
+                }
+            });
+    }
+}
