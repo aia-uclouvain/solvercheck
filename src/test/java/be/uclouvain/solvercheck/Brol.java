654835611007793330473067602573596a407efb
Xavier Gillard <xavier.gillard@uclouvain.be>
2018-09-21 00:53:38 +0200

    tue 92% des mutants

--- src/test/java/be/uclouvain/solvercheck/Brol.java
+++ src/test/java/be/uclouvain/solvercheck/Brol.java
@@ -1,7 +1,13 @@
 package be.uclouvain.solvercheck;
 
+import be.uclouvain.solvercheck.consistencies.ArcConsitency;
+import be.uclouvain.solvercheck.core.task.Checker;
+import be.uclouvain.solvercheck.core.task.DomainFilter;
 import be.uclouvain.solvercheck.stateful.StatefulFilterAdapter;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.function.Function;
 
 public class Brol implements WithSolverCheck {
 
@@ -50,6 +56,25 @@ public class Brol implements WithSolverCheck {
                 a(stateful(arcConsistent(allDiff())))
                         .isStrongerThan(stateful(rangeConsistent(allDiff())))
                         .forAll(partialAssignments().withValuesRanging(0, 3))
+        );
+    }
+
+    @Test
+    public void testHybridEquivalentToNormalConsistency() {
+        assertThat(
+                forAll(integers().between(0, 5))
+                .itIsTrueThat(size -> {
+                    Function<Checker, DomainFilter>[] filters =
+                            new Function[size];
+
+                    for (int i =  0; i < size; i++) {
+                        filters[i] = ArcConsitency::domainFilter;
+                    }
+
+                    return an(arcConsistent(allDiff()))
+                            .isEquivalentTo(hybrid(allDiff(), filters))
+                            .assuming(pa -> pa.size() == size);
+                })
         );
     }
 
