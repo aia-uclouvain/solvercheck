c0fcbdb0e13946f8fc4693048f10010fb8c7e7d1
Xavier Gillard <xavier.gillard@uclouvain.be>
2018-09-25 18:35:38 +0200

    tests for range consistency

---
+++ src/test/java/be/uclouvain/solvercheck/consistencies/range/TestRangeConsistency.java
@@ -0,0 +1,179 @@
+package be.uclouvain.solvercheck.consistencies.range;
+
+import be.uclouvain.solvercheck.checkers.WithCheckers;
+import be.uclouvain.solvercheck.consistencies.WithConsistencies;
+import be.uclouvain.solvercheck.core.data.Assignment;
+import be.uclouvain.solvercheck.core.data.Domain;
+import be.uclouvain.solvercheck.core.data.PartialAssignment;
+import be.uclouvain.solvercheck.core.task.Checker;
+import be.uclouvain.solvercheck.core.task.Filter;
+import be.uclouvain.solvercheck.generators.WithCpGenerators;
+import be.uclouvain.solvercheck.utils.collections.CartesianProduct;
+import be.uclouvain.solvercheck.utils.collections.Range;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.quicktheories.QuickTheory;
+import org.quicktheories.WithQuickTheories;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static be.uclouvain.solvercheck.utils.relations.PartialOrdering.EQUIVALENT;
+import static be.uclouvain.solvercheck.utils.relations.PartialOrdering.STRONGER;
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Integer.MIN_VALUE;
+
+public class TestRangeConsistency
+        implements WithQuickTheories,
+        WithCpGenerators,
+        WithConsistencies,
+        WithCheckers {
+
+    private QuickTheory qt;
+    private Checker checker;
+    private Filter  filter;
+
+    @Before
+    public void setUp() {
+        qt = qt().withGenerateAttempts(10000);
+        checker= allDiff();
+        filter = rangeConsistent(checker);
+    }
+
+    /**
+     * 1. It must be weakly-monotonic
+     * <i>
+     * A function p over domains is called weakly monotonic iff
+     *
+     *     a ∈ d ⇒ p({a}) ⊆ p(d) for all assignments a and domains d.
+     *
+     * A propagator is a contracting and weakly monotonic function over domains
+     * </i>
+     *
+     * ``Weakly Monotonic Propagators'' (Schulte, Tack, 2009)
+     */
+    @Test
+    public void itMustBeWeaklyMonotonic() {
+        qt.withExamples(10)
+          .forAll(integers().between(MIN_VALUE+5, MAX_VALUE-4))
+          .checkAssert(anchor ->
+            qt.withExamples(100)
+              .forAll(
+                  partialAssignments()
+                    .withValuesRanging(anchor-5, anchor+4)
+                    .withUpToVariables(5))
+                .assuming(pa -> !pa.isError())
+                .checkAssert(pa -> {
+                        PartialAssignment filtered = filter.filter(pa);
+
+                        // subseteq test
+                        Assert.assertTrue(List.of(STRONGER, EQUIVALENT).contains(filtered.compareWith(pa)));
+                    })
+          );
+    }
+
+    /**
+     * 2. It must be the least fixpoint (hence an extra round of propagation
+     * will not prune any additional value).
+     */
+    @Test
+    public void itMustBeTheLeastFixpoint() {
+        qt.withExamples(10)
+          .forAll(integers().between(MIN_VALUE+5, MAX_VALUE-4))
+          .checkAssert(anchor ->
+            qt.withExamples(100)
+              .forAll(
+                  partialAssignments()
+                    .withValuesRanging(anchor-5, anchor+4)
+                    .withUpToVariables(5))
+                .assuming(pa -> !pa.isError())
+                .checkAssert(pa -> {
+                    PartialAssignment filtered  = filter.filter(pa);
+                    PartialAssignment filtered2 = filter.filter(filtered);
+
+                    Assert.assertEquals(EQUIVALENT, filtered.compareWith(filtered2));
+                })
+          );
+    }
+
+    /**
+     * 3. It removes no solution
+     */
+    @Test
+    public void itRemovesNoSolution() {
+        qt.withExamples(10)
+          .forAll(integers().between(MIN_VALUE+5, MAX_VALUE-4))
+          .checkAssert(anchor ->
+             qt.withExamples(100)
+               .forAll(
+                  partialAssignments()
+                     .withValuesRanging(anchor-5, anchor+4)
+                     .withUpToVariables(5))
+               .assuming(pa -> !pa.isError())
+               .checkAssert(pa -> {
+
+                 PartialAssignment filtered = filter.filter(pa);
+
+                   PartialAssignment solutions =
+                           PartialAssignment.unionOf(pa.size(),
+                                   CartesianProduct.of(pa)
+                                           .stream()
+                                           .filter(a -> checker.test(Assignment.from(a)))
+                                           .collect(Collectors.toList())
+                           );
+
+                 boolean error = solutions.isError();
+                 boolean solsOk= List.of(STRONGER, EQUIVALENT)
+                         .contains(solutions.compareWith(filtered));
+
+                 Assert.assertTrue(error || solsOk);
+             })
+          );
+    }
+
+    /**
+     * 4. All values of all domains, must have a bound support
+     */
+    @Test
+    public void testConsistencyDefinition() {
+        qt.withExamples(10)
+          .forAll(integers().between(MIN_VALUE+5, MAX_VALUE-4))
+          .checkAssert(anchor ->
+           qt.withExamples(100)
+             .forAll(
+                partialAssignments()
+                  .withValuesRanging(anchor-5, anchor+4)
+                  .withUpToVariables(5))
+                .assuming(pa -> !pa.isError())
+                .checkAssert(pa -> {
+                    PartialAssignment filtered  = filter.filter(pa);
+
+                    CartesianProduct<Integer> possibilities =
+                            CartesianProduct.of(pa.stream().map(dom ->
+                                     Range.between(
+                                        (long) dom.minimum(),
+                                        (long) dom.maximum()+1))
+                                    .collect(Collectors.toList()));
+
+                    if (!filtered.isError()) {
+                        int arity = filtered.size();
+
+                        for (int i = 0; i < arity; i++) {
+                            final int var = i;
+                            final Domain domain = filtered.get(var);
+
+                            for(int value : domain) {
+                                boolean support =
+                                  possibilities.stream().anyMatch(ass ->
+                                       ass.get(var).equals(value)
+                                    && checker.test(Assignment.from(ass))
+                                  );
+                                Assert.assertTrue(support);
+                            }
+                        }
+                    }
+                })
+          );
+    }
+}
---
+++ src/test/java/be/uclouvain/solvercheck/consistencies/range/TestRangeConsistencyDomainFilter.java
@@ -0,0 +1,148 @@
+package be.uclouvain.solvercheck.consistencies.range;
+
+import be.uclouvain.solvercheck.checkers.WithCheckers;
+import be.uclouvain.solvercheck.consistencies.RangeConsistency;
+import be.uclouvain.solvercheck.consistencies.WithConsistencies;
+import be.uclouvain.solvercheck.core.data.Assignment;
+import be.uclouvain.solvercheck.core.data.Domain;
+import be.uclouvain.solvercheck.core.data.PartialAssignment;
+import be.uclouvain.solvercheck.core.task.Checker;
+import be.uclouvain.solvercheck.core.task.DomainFilter;
+import be.uclouvain.solvercheck.generators.WithCpGenerators;
+import be.uclouvain.solvercheck.utils.collections.CartesianProduct;
+import be.uclouvain.solvercheck.utils.collections.Range;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.quicktheories.QuickTheory;
+import org.quicktheories.WithQuickTheories;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static be.uclouvain.solvercheck.utils.relations.PartialOrdering.EQUIVALENT;
+import static be.uclouvain.solvercheck.utils.relations.PartialOrdering.STRONGER;
+
+public class TestRangeConsistencyDomainFilter
+        implements WithQuickTheories,
+                   WithCpGenerators,
+                   WithConsistencies,
+                   WithCheckers {
+
+    private QuickTheory qt;
+    private Checker checker;
+    private DomainFilter filter;
+
+    @Before
+    public void setUp() {
+        qt = qt().withGenerateAttempts(10000);
+        checker= allDiff();
+        filter = RangeConsistency.domainFilter(checker);
+    }
+
+    /**
+     * 1. It must be weakly-monotonic
+     * <i>
+     * A function p over domains is called weakly monotonic iff
+     *
+     *     a ∈ d ⇒ p({a}) ⊆ p(d) for all assignments a and domains d.
+     *
+     * A propagator is a contracting and weakly monotonic function over domains
+     * </i>
+     *
+     * ``Weakly Monotonic Propagators'' (Schulte, Tack, 2009)
+     */
+    @Test
+    public void itMustBeWeaklyMonotonic() throws Exception {
+        qt.withExamples(10)
+          .forAll(integers().between(Integer.MIN_VALUE+5, Integer.MAX_VALUE-4))
+          .checkAssert(anchor ->
+            qt.withExamples(100)
+              .forAll(
+                    partialAssignments()
+                            .withUpToVariables(5)
+                            .withValuesRanging(anchor-5, anchor+4))
+              .assuming(pa -> !pa.isError())
+              .checkAssert(pa -> {
+                  for (int var = 0; var < pa.size(); var++) {
+                      Domain filtered = filter.filter(var, pa);
+
+                      Assert.assertTrue(List.of(STRONGER, EQUIVALENT)
+                            .contains(filtered.compareWith(pa.get(var))));
+                  }
+              })
+        );
+    }
+
+    /**
+     * 2. It removes no solution
+     */
+    @Test
+    public void itRemovesNoSolution() {
+        qt.withExamples(10)
+          .forAll(integers().between(Integer.MIN_VALUE+5, Integer.MAX_VALUE-4))
+          .checkAssert(anchor ->
+             qt.withExamples(100)
+               .forAll(
+                   partialAssignments()
+                           .withUpToVariables(5)
+                           .withValuesRanging(anchor-5, anchor+4))
+               .assuming(pa -> !pa.isError())
+               .checkAssert(pa -> {
+                   PartialAssignment solutions =
+                      PartialAssignment.unionOf(pa.size(),
+                         CartesianProduct.of(pa)
+                            .stream()
+                            .filter(a -> checker.test(Assignment.from(a)))
+                            .collect(Collectors.toList())
+                      );
+                   for (int i = 0; i < pa.size(); i++) {
+                       Domain filtered = filter.filter(i, pa);
+                       Assert.assertTrue(filtered.containsAll(solutions.get(i)));
+                   }
+               })
+          );
+    }
+
+    /**
+     * 3. All values of all domains, must have a bound support
+     */
+    @Test
+    public void testConsistencyDefinition() {
+        qt.withExamples(10)
+          .forAll(integers().between(Integer.MIN_VALUE+5, Integer.MAX_VALUE-4))
+          .checkAssert(anchor ->
+             qt.withExamples(100)
+               .forAll(
+                  partialAssignments()
+                     .withUpToVariables(5)
+                     .withValuesRanging(anchor-5, anchor+4))
+               .assuming(pa -> !pa.isError())
+               .checkAssert(pa -> {
+                   CartesianProduct<Integer> boundSupports =
+                           CartesianProduct.of(
+                               pa.stream().map(d ->
+                                   Range.between(
+                                           (long) d.minimum(),
+                                           (long) d.maximum() + 1))
+                           .collect(Collectors.toList()));
+
+                   for (int i = 0; i < pa.size(); i++) {
+                       final int var = i;
+                       final Domain filtered = filter.filter(var, pa);
+
+                       if (!filtered.isEmpty()) {
+                           for(int value : filtered) {
+                               boolean support =
+                                   boundSupports.stream().anyMatch(ass ->
+                                        ass.get(var).equals(filtered.minimum())
+                                     && checker.test(Assignment.from(ass))
+                               );
+                               Assert.assertTrue(support);
+                           }
+                       }
+                   }
+               })
+      );
+    }
+}
